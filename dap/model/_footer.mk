cb85beef2e4aa5501f85c0c4181ea39dfd048e52
Ivan Molineris <ivan.molineris@unito.it>
2023-01-18 12:17:54 +0100

    Fixed the inclusion of _header.mk and _footer.mk, debug of bmakefile inclusion

--- dap/model/_footer.mk
+++ dap/model/_footer.mk
@@ -1,5 +1,11 @@
 
-.bmake/makefile_versioned.mk: bmakefile_versioned.mk ; @bmake-filter -o --ignore-global-rules  < $< > $@
+ifneq (,$(wildcard ./bmakefile_versioned.mk))
+.bmake/makefile_versioned.mk: bmakefile_versioned.mk
+	@echo bmakefile_versioned.mk uptate detected, compling.
+	mkdir -p .bmake
+	bmake-filter -o --ignore-global-rules $(PWD) < $< > $@
+	@echo bmake compiling done
+endif
 -include .bmake/epigen/makefile_versioned.mk 
 -include makefile_versioned.mk
-#-include ignore dhe include if the file does not exists
+#-include ignore the include if the file does not exists
--- dap/model/_header.mk
+++ dap/model/_header.mk
@@ -3,14 +3,18 @@ SHELL=/bin/bash -eo pipefail
 
 .DELETE_ON_ERROR:
 
-.bmake/config.mk: config_bmake.mk ; @bmake-filter -o --ignore-global-rules  < $< > $@
--include .bmake/config.mk 
 -include config.mk
 #-include ignore che include if the file does not exists
 
 
-.bmake/makefile: bmakefile ; @bmake-filter -o --ignore-global-rules  < $< > $@
--include .bmake/makefile
-#-include ignore che include if the file does not exists
+ifneq (,$(wildcard ./bmakefile))
+.bmake/makefile: bmakefile
+	@echo bmakefile uptate detected, compling.
+	mkdir -p .bmake
+	bmake-filter -o --ignore-global-rules $(PWD) < $< > $@
+	@echo bmake compiling done
+endif
+include .bmake/makefile
+#-include ignore the include if the file does not exists
 
 
