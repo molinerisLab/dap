d43cf941c81c95af683571c27a6797ab4a927bcf
Marco-Masera <mirco.masera@gmail.com>
2024-12-17 11:36:36 +0100

    .

--- conda_recipe/cook_recipe.sh
+++ conda_recipe/cook_recipe.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 conda-build .
-anaconda upload --user molinerislab $HOME/anaconda3/conda-bld/noarch/dap-0.5-py_0.tar.bz2
+anaconda upload --user molinerislab $HOME/.conda/envs/mmasera_env/conda-bld/noarch/dap-0.6-py_0.tar.bz2
 
 #conda convert --platform all $HOME/anaconda3/conda-bld/linux-64/dap-0.3-py_0.tar.bz2 -o outputdir/
 #anaconda upload --user molinerislab outputdir/linux-32/dap-0.3-py_0.tar.bz2
